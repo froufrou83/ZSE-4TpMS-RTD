57ac488bbfded528f51ced0f96d2a06017d2175e
froufrou83 <priv.ph313@gmail.com>
2025-12-11 14:17:45 +0100

    Add Sphinx configuration for documentation

---
+++ docs/conf.py
@@ -0,0 +1,18 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+project = 'Read the docs'
+author = 'Maciej Stępień'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.todo',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
